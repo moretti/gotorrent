97b26259a070bfd36fad33a97ebafe0df13ce0c5
Paolo Moretti <paulmcpazzi@gmail.com>
2013-07-11 16:11:23 -0400

    Added some comments to remember the general idea of this struct

--- file.go
+++ file.go
@@ -1,4 +1,10 @@
 package gotorrent
 
+// The File struct is used by Torrent struct to read/write from files.
+// The torrent will create one for every file in the torrent upon initialization.
 type File struct {
+}
+
+func (file *File) Write(pieceOffset int, data []byte) {
+	panic("Not implemented")
 }
